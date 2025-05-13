8036d7a3f73b6658e14c55f8e2ce687da7199308
youjoon <youjoon@eluocnc.com>
2025-05-13 23:02:20 +0900

    [프로그레스바] 돔 동적 생성

--- assets/js/ui/ui_progressbar.js
+++ assets/js/ui/ui_progressbar.js
@@ -3,26 +3,34 @@ class Progressbar {
     this.el = document.querySelector('.scroll-progress');
     const defaultOptions = {type: 'bar', isLabel: 'true'}
     this.options = Object.assign({}, defaultOptions, JSON.parse(this.el.dataset.progressbarOptions || '{}'));
+    this.bar = null;
     this.label = null;
+    this.scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
     this.init();
   }
   init () {
+    this.createBar();
     this.scrollEvent();
+  }
+  createBar () {
+    this.bar = document.createElement('span');
+    this.el.appendChild(this.bar);
+    this.bar.classList.add('scroll-progress__bar');
+    this.bar.style.width = Math.floor(window.scrollY / this.scrollHeight * 100);
   }
   activateLabel () {
     this.label = document.createElement('span');
     this.el.appendChild(this.label)
-    this.label.classList.add('.scroll-progress__label');
-    this.label.textContent = Math.floor(window.scrollY / scrollHeight * 100)
+    this.label.classList.add('scroll-progress__label');
+    this.label.textContent = Math.floor(window.scrollY / this.scrollHeight * 100);
   }
   scrollEvent () {
-    const scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
-    const bar = this.el.querySelector('.scroll-progress__bar');
-    this.activateLabel()
+    if(this.options.isLabel) this.activateLabel();
+
     document.addEventListener('scroll', () => {
-      const progress = Math.floor(window.scrollY / scrollHeight * 100)
-      label.textContent = progress;
-      bar.style.width = `${progress}%`;
+      const progress = Math.floor(window.scrollY / this.scrollHeight * 100);
+      if(this.options.isLabel) this.label.textContent = progress;
+      this.bar.style.width = `${progress}%`;
     })
   }
 }
